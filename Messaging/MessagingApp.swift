d00f84ec846fa1b11b8d1085dbbcf96d797e59f0
Dounia Belannab <dbelannab@gmail.com>
2022-12-09 15:21:10 +0100

    Initial Commit

---
+++ Messaging/MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingApp.swift
+//  Messaging
+//
+//  Created by Dounia Belannab on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
